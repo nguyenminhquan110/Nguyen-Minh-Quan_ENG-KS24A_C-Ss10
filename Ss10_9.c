45d432d5390a1b9b0be4fa8dd23b23ad79cb344f
nguyenminhquan110 <nmquan11026@gmail.com>
2024-12-03 13:40:29 +0700

    Add files via upload

--- Ss10_9.c
+++ Ss10_9.c
@@ -1,33 +1,40 @@
-#include<stdio.h>
-
-int main(){
-
-int num[3][3]={{1,4,3},{5,2,9},{7,8,6}};
-    int b[9]={0};
-
-	for (int i = 0; i < 3; i++) {
-		b[i] = num[i][i];
+#include <stdio.h>
+int main() {
+    int n, m;
+    printf("Nhap so dong: ");
+    scanf("%d", &n);
+    printf("Nhap so cot: ");
+    scanf("%d", &m);
+    int arr[n][m];
+    printf("Nhap cac phan tu cua mang:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+        printf("------\n");
     }
-	for(int i = 0;i < 3;i++){
-		for(int j = 0;j < (3 - 1 - i);j++){
-			if(b[j + 1] < b[j]){
-				int temp = b[j];
-				b[j] = b[j+1];
-				b[j+1] = temp;
-			}
-		}
+	int min;
+	if (n < m) {
+	    min = n;
+	} else {
+	    min = m;
 	}
-	for (int i = 0; i < 3; i++) {
-        num[i][i] =b[i];
+    for (int i = 0; i < min - 1; i++) {
+        for (int j = 0; j < min - i - 1; j++) {
+            if (arr[j][j] > arr[j + 1][j + 1]) {
+                int temp = arr[j][j];
+                arr[j][j] = arr[j + 1][j + 1];
+                arr[j + 1][j + 1] = temp;
+            }
+        }
     }
-    for(int i = 0;i < 3;i++){
-    	for(int j = 0;j < 3;j++){
-    		printf("%d",num[i][j]);
-		}
-		printf("\n");
-	}   
-    
-
-	return 0;
+    printf("Mang sau khi sap xep theo duong cheo chinh:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
 
